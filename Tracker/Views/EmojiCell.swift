1f2168cd5a38e5c8e3752a364ff0ae3a0f4ff2b1
NatAn <andrei.v.0204@gmail.com>
2025-09-01 16:30:37 +0300

    Добавлена коллекция эмоджи и цветов

---
+++ Tracker/Views/EmojiCell.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+final class EmojiCell: UICollectionViewCell {
+    private let emojiLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(emojiLabel)
+        emojiLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            emojiLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            emojiLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    func configure(with emoji: String, isSelected: Bool) {
+        emojiLabel.text = emoji
+        
+        if isSelected {
+            contentView.backgroundColor = UIColor(resource: .ypLightGray)
+            contentView.layer.cornerRadius = 16
+        } else {
+            contentView.backgroundColor = .clear
+            contentView.layer.cornerRadius = 0
+        }
+    }
+}
