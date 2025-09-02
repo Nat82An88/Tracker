059e48221b31e0e4e5f81b7feebf0145b51cff34
NatAn <andrei.v.0204@gmail.com>
2025-09-02 17:13:01 +0300

    Создал модель схемы CoreData

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -1,9 +1,13 @@
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        WeekdayArrayTransformer.register()
+        
         return true
     }
     
