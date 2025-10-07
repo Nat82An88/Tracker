8a8c1437fd29dc7db9dfa96b7177f22122d0a55d
NatAn <andrei.v.0204@gmail.com>
2025-10-07 15:58:08 +0300

    Тесты для светлой и темной темы проходят успешно

--- TrackerTests/SnapshotTest.swift
+++ TrackerTests/SnapshotTest.swift
@@ -4,62 +4,7 @@ import SnapshotTesting
 
 final class TrackersViewControllerSnapshotTests: XCTestCase {
     
-    func testMainScreen() {
-        // Given
-        let vc = TrackersViewController()
-        
-        // When
-        vc.loadViewIfNeeded()
-        
-        // Then
-        assertSnapshot(of: vc, as: .image, named: "main_screen", record: false)
-    }
-    
     func testMainScreenLightMode() {
-        // Given
-        let vc = TrackersViewController()
-        
-        // When
-        vc.loadViewIfNeeded()
-        
-        // Then
-        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .light)), named: "main_screen_light", record: false)
-    }
-    
-    func testMainScreenWithDatePickerExpanded() {
-        // Given
-        let vc = TrackersViewController()
-        
-        // When
-        vc.loadViewIfNeeded()
-        
-        // Then
-        assertSnapshot(of: vc, as: .image, named: "main_screen_date_picker", record: false)
-    }
-    
-    func testMainScreenEmptyState() {
-        // Given
-        let vc = TrackersViewController()
-        
-        // When
-        vc.loadViewIfNeeded()
-        
-        // Then
-        assertSnapshot(of: vc, as: .image, named: "main_screen_empty", record: false)
-    }
-    
-    func testMainScreenWithTrackers() {
-        // Given
-        let vc = TrackersViewController()
-        
-        // When
-        vc.loadViewIfNeeded()
-        
-        // Then
-        assertSnapshot(of: vc, as: .image, named: "main_screen_with_trackers", record: false)
-    }
-    
-    func testMainScreenAccessibilityLargeText() {
         // Given
         let vc = TrackersViewController()
         
@@ -69,8 +14,120 @@ final class TrackersViewControllerSnapshotTests: XCTestCase {
         // Then
         assertSnapshot(
             of: vc,
-            as: .image(traits: .init(preferredContentSizeCategory: .accessibilityLarge)),
-            named: "main_screen_large_text",
+            as: .image(traits: .init(userInterfaceStyle: .light)),
+            named: "main_screen_light",
+            record: false
+        )
+    }
+    
+    func testMainScreenDarkMode() {
+        // Given
+        let vc = TrackersViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .dark)),
+            named: "main_screen_dark",
+            record: false
+        )
+    }
+    
+    func testMainScreenWithDatePickerExpandedLightMode() {
+        // Given
+        let vc = TrackersViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .light)),
+            named: "main_screen_date_picker_light",
+            record: false
+        )
+    }
+    
+    func testMainScreenWithDatePickerExpandedDarkMode() {
+        // Given
+        let vc = TrackersViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .dark)),
+            named: "main_screen_date_picker_dark",
+            record: false
+        )
+    }
+    
+    func testMainScreenEmptyStateLightMode() {
+        // Given
+        let vc = TrackersViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .light)),
+            named: "main_screen_empty_light",
+            record: false
+        )
+    }
+    
+    func testMainScreenEmptyStateDarkMode() {
+        // Given
+        let vc = TrackersViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .dark)),
+            named: "main_screen_empty_dark",
+            record: false
+        )
+    }
+    
+    func testMainScreenWithTrackersLightMode() {
+        // Given
+        let vc = TrackersViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .light)),
+            named: "main_screen_with_trackers_light",
+            record: false
+        )
+    }
+    
+    func testMainScreenWithTrackersDarkMode() {
+        // Given
+        let vc = TrackersViewController()
+        
+        // When
+        vc.loadViewIfNeeded()
+        
+        // Then
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .dark)),
+            named: "main_screen_with_trackers_dark",
             record: false
         )
     }
--- TrackerTests/SnapshotValidationTest.swift
+++ TrackerTests/SnapshotValidationTest.swift
@@ -4,7 +4,7 @@ import SnapshotTesting
 
 final class SnapshotValidationTest: XCTestCase {
     
-    func testSnapshotFailsWhenBackgroundChanges() {
+    func testSnapshotFailsWhenBackgroundChangesLightMode() {
         // Given
         let vc = TrackersViewController()
         vc.loadViewIfNeeded()
@@ -13,10 +13,32 @@ final class SnapshotValidationTest: XCTestCase {
         vc.view.backgroundColor = .systemRed
         
         // Then: тест ДОЛЖЕН провалиться (это проверка что тесты работают)
-        assertSnapshot(of: vc, as: .image, named: "should_fail_with_red_background", record: false)
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .light)),
+            named: "should_fail_with_red_background_light",
+            record: false
+        )
     }
     
-    func testSnapshotFailsWhenTitleChanges() {
+    func testSnapshotFailsWhenBackgroundChangesDarkMode() {
+        // Given
+        let vc = TrackersViewController()
+        vc.loadViewIfNeeded()
+        
+        // When:
+        vc.view.backgroundColor = .systemRed
+        
+        // Then: тест ДОЛЖЕН провалиться (это проверка что тесты работают)
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .dark)),
+            named: "should_fail_with_red_background_dark",
+            record: false
+        )
+    }
+    
+    func testSnapshotFailsWhenTitleChangesLightMode() {
         // Given
         let vc = TrackersViewController()
         vc.loadViewIfNeeded()
@@ -25,6 +47,28 @@ final class SnapshotValidationTest: XCTestCase {
         vc.title = "Измененный заголовок"
         
         // Then: тест ДОЛЖЕН провалиться
-        assertSnapshot(of: vc, as: .image, named: "should_fail_with_changed_title", record: false)
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .light)),
+            named: "should_fail_with_changed_title_light",
+            record: false
+        )
+    }
+    
+    func testSnapshotFailsWhenTitleChangesDarkMode() {
+        // Given
+        let vc = TrackersViewController()
+        vc.loadViewIfNeeded()
+        
+        // When:
+        vc.title = "Измененный заголовок"
+        
+        // Then: тест ДОЛЖЕН провалиться
+        assertSnapshot(
+            of: vc,
+            as: .image(traits: .init(userInterfaceStyle: .dark)),
+            named: "should_fail_with_changed_title_dark",
+            record: false
+        )
     }
 }
